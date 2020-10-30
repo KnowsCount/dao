f7f9788a6328c39f214511728a8ab85a501bb651
KnowsCount <knowscount@gmail.com>
2020-10-31 00:14:36 +0800

    tab size & localisation of typical

--- Scripts/type.js
+++ Scripts/type.js
@@ -1,3 +1,4 @@
-import { type, type as loop } from 'https://unpkg.com/@camwiegert/typical';
+// import { type, type as loop } from 'https://unpkg.com/@camwiegert/typical';
+import { type, type as loop } from './type-im.js';
 
 type(target, 'KnowsCount', 1000, '大道至简', 1000, loop);
